d183073d9c57eb6bd617274e68e322078cea2ae7
Almaz Salikhov <almaz.salikhov@gmail.com>
2023-01-01 20:28:20 +0200

    HW task 1 solved

---
+++ HomeWork/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+string NumbersFromNto1(int N)
+{
+    if (N == 1)
+    {
+        // Console.WriteLine("YES: " + N);
+        return "1";
+    }
+    else
+    {
+        // Console.WriteLine("NO: " + N);
+        return (N + ", ") + NumbersFromNto1(N - 1);
+    }
+}
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Натуральные числа от " + (num) + " до 1: " + (NumbersFromNto1(num)));
