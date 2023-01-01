ffd9246a804e0fe7f2016b665e117ea96ee7275d
Almaz Salikhov <almaz.salikhov@gmail.com>
2023-01-01 21:53:11 +0200

    HW task 2: variation 2

---
+++ HomeWork/task2_var2/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Задайте значения M и N
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+int SumOfNumbersfromNtoM(int max, int min)
+{
+    {
+        return (max == min ? max : max + SumOfNumbersfromNtoM(max - 1, min));
+    }
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0 || M < 0)
+{
+    Console.WriteLine("Одно из чисел, либо оба — не натуральное");
+}
+else
+{
+    // сначала выясняю, какое число больше
+    // после этого завожу в функцию SumOfNumbersfromNtoM
+    // чтобы не разветвлять ее внутри
+    int max = N;
+    int min = M;
+    if (N < M)
+    {
+        max = M;
+        min = N;
+    }
+    Console.WriteLine((M == N) ? "Числа совпадают" : SumOfNumbersfromNtoM(max, min));
+}
+
+
