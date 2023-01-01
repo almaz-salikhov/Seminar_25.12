5ea0f515143387ce965e4a6cb32ded4423977876
Almaz Salikhov <almaz.salikhov@gmail.com>
2023-01-01 21:37:08 +0200

    HW task 2 solved

--- HomeWork/task2/Program.cs
+++ HomeWork/task2/Program.cs
@@ -2,8 +2,28 @@
 // Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
 // Выполнить с помощью рекурсии.
 
+int SumfromNtoM(int n, int m)
+{
+    if (n > m)
+    {
+        return (n == m ? n : n + SumfromNtoM(n - 1, m));
+    }
+    else
+    {
+        return (n == m ? m : m + SumfromNtoM(n, m - 1));
+    }
+}
+
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-
 Console.Write("Введите число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0 || M < 0)
+{
+    Console.WriteLine("Одно из чисел, либо оба — не натуральное");
+}
+else
+{
+    Console.WriteLine((M == N) ? "Числа совпадают" : SumfromNtoM(N, M));
+}
