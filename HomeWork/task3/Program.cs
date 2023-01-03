047dbc2b5ce8e92c21fd612e0d3cc083b9464c3c
Almaz Salikhov <almaz.salikhov@gmail.com>
2023-01-03 18:32:41 +0200

    Task 3 Akkerman done with ternary operator

---
+++ HomeWork/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int Akk(int m, int n)
+{
+    int res = 0;
+    if (m == 0)
+    {
+        res = n + 1;
+    }
+    if (m > 0 & n == 0)
+    {
+        res = Akk(m - 1, 1);
+    }
+    if (m > 0 & n > 0)
+    {
+        res = Akk(m - 1, Akk(m, n - 1));
+    }
+    return res;
+}
+
+Console.WriteLine (n < 0 || m < 0 ? "Одно из чисел, либо оба — отрицательные" : Akk(m, n));
